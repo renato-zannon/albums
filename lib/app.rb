045b4681880bcb54a7134869d0d452546c332f6e
Renato Zannon <renato.riccieri@gmail.com>
2012-11-25 12:42:38 -0500

    Configure static files lookup

--- lib/app.rb
+++ lib/app.rb
@@ -4,6 +4,12 @@ require_relative "album"
 
 helpers Sinatra::JSON
 
+set :public_folder, "www/"
+
+get '/' do
+  send_file File.join(settings.public_folder, "index.html")
+end
+
 get '/albums/:artist_name' do
   albums = Album.of_artist(params[:artist_name])
   json albums.map(&:as_json)
